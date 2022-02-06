6dff996ad6ac783f5628ba8158e8ea8d861e3e9c
xale08 <48703832+xale08@users.noreply.github.com>
2022-02-05 23:57:31 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Funnel } from "funnel-react";
+
+import "./styles.css";
+
+const data = [
+  {
+    _id: "5de52b4ac4275a463f912042",
+    item: "accepted",
+    label: "Crear Usuario",
+    percent: 100,
+    quantity: 36473
+  },
+  {
+    _id: "5de52b4ac4275a463f912041",
+    item: "visitors",
+    label: "Verificación Cel",
+    percent: 97,
+    quantity: 35522
+  },
+  {
+    _id: "5de52b4ac4275a463f912040",
+    item: "postulants",
+    label: "Info Personal",
+    percent: 71,
+    quantity: 25797
+  },
+  {
+    _id: "5de52b4ac4275a463f91203f",
+    item: "inprocess",
+    label: "Domicilio",
+    percent: 71,
+    quantity: 25806
+  },
+  {
+    _id: "5de52b4ac4275a463f91203e",
+    item: "finalists",
+    label: "Firma Contrato",
+    percent: 62,
+    quantity: 22701
+  }
+];
+
+function App() {
+  return (
+    <div className="App">
+      <Funnel
+        labelKey="label"
+        height={252}
+        colors={["#1890FF", "#BAE7FF"]}
+        colorPercent="#fff"
+        colorLabel="#fff"
+        valueKey="quantity"
+        width={800}
+        displayPercent={true}
+        data={data}
+      />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
